80f1548b55038d6f84b4615324a5426c43e44306
菊池潤 <kikuchi753ku@gmail.com>
2024-01-02 23:33:45 +0900

    最新状態です

---
+++ app/Models/Calendars/ReserveSettingUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Calendars;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReserveSettingUser extends Model
+{
+    //
+    protected $table = 'reserve_setting_users';
+}
